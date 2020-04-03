209e204e5754bf15085dff6b9706616581fd7f5a
holymoly <holymoly@Maurices-MacBook-Pro.local>
2020-04-04 00:08:56 +0200

    call api, dynamically rooms and lights

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// User defined imports
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+
+import { ReactiveFormsModule } from "@angular/forms";
+import { LightComponent } from './light/light.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, LightComponent, DashboardComponent, RoomComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
