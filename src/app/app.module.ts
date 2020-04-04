6432639edbfb3f0aa0db1621a03642c77c882f75
holymoly <holymoly@Maurices-MacBook-Pro.local>
2020-04-04 12:59:43 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,33 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { LoginComponent } from "./login/login.component";
 
-import { ReactiveFormsModule } from "@angular/forms";
-import { LightComponent } from './light/light.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { RoomComponent } from './room/room.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LightComponent } from "./light/light.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { RoomComponent } from "./room/room.component";
+
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LightComponent, DashboardComponent, RoomComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LightComponent,
+    DashboardComponent,
+    RoomComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
